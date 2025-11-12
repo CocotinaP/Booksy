79f3917f5be4fcb2687a5a4f376ba02c591211b2
vdiana16 <valentina.pasca@stud.ubbcluj.ro>
2025-11-12 14:22:49 +0200

    Am adaugat end point pentru register fixes #11

---
+++ booksy_back/booksy_server/views/views_register.py
@@ -0,0 +1,30 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from booksy_server.models.user import User
+from booksy_server.serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        access = refresh.access_token
+        return Response({
+            "message": "User registered successfully!",
+            "refresh": str(refresh),
+            "access": str(access),
+            "user": {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "phone_number": user.phone_number,
+                "address": user.address,
+            }
+        }, status=status.HTTP_201_CREATED)
