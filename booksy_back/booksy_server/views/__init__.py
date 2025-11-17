b11fed4459536d4eab94cd3508f9101892a9d4f2
Cosmina <cosmipopovici05@yahoo.com>
2025-11-17 19:59:22 +0200

    Am facut end-point pentru lista de carti (crud operations)

--- booksy_back/booksy_server/views/__init__.py
+++ booksy_back/booksy_server/views/__init__.py
@@ -1,3 +1,4 @@
 from .hello_world import HelloWorldView
 from .views_auth import CustomTokenObtainPairView
 from .views_books import BookViewSet
+from .views_book_announcement import BookAnnouncementViewSet
