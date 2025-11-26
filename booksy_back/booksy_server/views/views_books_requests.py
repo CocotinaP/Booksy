623a53ce996b1b5b842104b163e02677317ddb05
vdiana16 <valentina.pasca@stud.ubbcluj.ro>
2025-11-26 11:03:49 +0200

    Am adaugat endpoint pentru anularea unei cereri de inchiriere

--- booksy_back/booksy_server/views/views_books_requests.py
+++ booksy_back/booksy_server/views/views_books_requests.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets, permissions, serializers
+from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.utils import timezone
+from datetime import timedelta
 from ..models.book_request import BookRequest
 from ..serializers import BookRequestSerializer
 
@@ -47,3 +49,56 @@ class BookRequestViewSet(viewsets.ModelViewSet):
         book_request.status = 'rejected'
         book_request.save()
         return Response({'status': 'rejected'})
+    
+    @action(detail=True, methods=['put'], url_path='cancel')
+    def cancel(self, request, pk=None):
+        book_request = self.get_object()
+        user_to_penalize = book_request.requester
+        
+        if user_to_penalize != request.user:
+            return Response(
+                {'error': 'Not authorized.'}, 
+                status=status.HTTP_403_FORBIDDEN
+            )
+            
+        if book_request.status in ['rejected', 'returned', 'canceled']:
+            return Response(
+                {'error': f"The book request cannot be canceled because the current status is '{book_request.status}'."}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # VERIFICARE TERMEN LIMITĂ (Aplicabilă pentru 'pending' ȘI 'accepted')
+        today = timezone.now().date()
+        cancellation_deadline = book_request.start_date - timedelta(days=3)
+
+        if today > cancellation_deadline:
+             return Response(
+                {
+                    'error': f"Cancellation of the request is no longer allowed. You must cancel at least 3 days before the start of the rental period. The deadline has been {cancellation_deadline}."
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+                
+        is_accepted = book_request.status == 'accepted'
+        
+        book_request.status = 'canceled'
+        book_request.save()
+
+        # Aplicare Penalizare DOAR dacă a fost ACCEPTED anterior
+        if is_accepted:
+            PENALTY_AMOUNT = 5.0 
+            
+            user_to_penalize.rating = max(0.0, user_to_penalize.rating - PENALTY_AMOUNT)
+            user_to_penalize.save()
+            
+            message = f"The request has been canceled. You have received a penality of {PENALTY_AMOUNT} points to your trust rating ({user_to_penalize.rating})."
+        else:
+            message = "The pending request has been canceled."
+
+        return Response(
+            {
+                'status': 'canceled', 
+                'message': message
+            }, 
+            status=status.HTTP_200_OK
+        )
