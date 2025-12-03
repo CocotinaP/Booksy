bb45ed64bde2b787b288a76ad4ea5fc8366eb7fc
vdiana16 <valentina.pasca@stud.ubbcluj.ro>
2025-12-03 14:59:52 +0200

    feat: Am adaugat endpoint pentru modul supriza

--- booksy_back/booksy_server/views/views_books.py
+++ booksy_back/booksy_server/views/views_books.py
@@ -1,4 +1,6 @@
-from rest_framework import serializers, viewsets, filters
+from rest_framework import serializers, viewsets, filters, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from ..models import Book
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -30,3 +32,38 @@ class BookViewSet(viewsets.ModelViewSet):
     # câmpuri pe care se poate ordona
     ordering_fields = ['price_per_day', 'title', 'author']
     ordering = ['title']  # ordonare implicită
+
+    @action(detail=False, methods=['get'])
+    def surprise(self, request):
+        """
+        Returnează o carte aleatorie dintr-un gen specificat.
+        Utilizare: /books/surprise/?genre=SF
+        """
+
+        genre_param = request.query_params.get('genre')
+
+        if not genre_param:
+            return Response(
+                {"error": "Please specify a genre (ex: ?genre=Fiction)"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        books = Book.objects.filter(
+            genre__icontains=genre_param, 
+            available=True
+        )
+
+        if request.user.is_authenticated:
+            books = books.exclude(owner=request.user)
+
+        if not books.exists():
+            return Response(
+                {"message": f"I couldn't find any books available in the genre '{genre_param}'."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    
+        random_book = books.order_by('?').first()
+
+        serializer = self.get_serializer(random_book)
+        return Response(serializer.data)
